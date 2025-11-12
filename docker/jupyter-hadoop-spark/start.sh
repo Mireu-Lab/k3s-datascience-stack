f7c1c72ffbef94a1a516d5040cab861fb3505747
mireu-lab <limmireu@cu.ac.kr>
2025-11-12 18:17:45 +0000

    📦 build: update dockerfile and start script
    - add pip freeze to generate a list of installed packages
    - update start script to create user workspace directory
    - copy package list to user workspace
    - start jupyterlab in user workspace

--- docker/jupyter-hadoop-spark/start.sh
+++ docker/jupyter-hadoop-spark/start.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
 # Startup script for Jupyter container
-# Clone Git repo if specified
 
 set -e
 
-REPO_URL=${GIT_REPO_URL:-https://github.com/Mireu-Lab/k3s-datascience-stack}
-REPO_NAME=${GIT_REPO_NAME:-default-repo}
-WORK_DIR="/home/jovyan/$REPO_NAME"
+# Default values
+USER_EMAIL=${JUPYTERHUB_USER:-jovyan}
 
-if [ ! -d "$WORK_DIR" ]; then
-    echo "Cloning repository $REPO_URL to $WORK_DIR"
-    git clone "$REPO_URL" "$WORK_DIR"
-else
-    echo "Repository already exists at $WORK_DIR"
-fi
+# Sanitize email for use as a directory name
+SAFE_EMAIL=$(echo "$USER_EMAIL" | tr '@.' '_')
 
-# Start JupyterLab
-exec jupyter-lab --allow-root --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token= --NotebookApp.password= --NotebookApp.base_url=${JUPYTERHUB_SERVICE_PREFIX} --NotebookApp.default_url=/lab
+# Define the workspace directory
+WORK_DIR="/home/jovyan/$SAFE_EMAIL"
+
+# Create the directory if it doesn't exist
+mkdir -p "$WORK_DIR"
+
+# Copy the package list to the workspace
+cp /opt/packages.txt "$WORK_DIR/packages.txt"
+
+# Start JupyterLab in the user's workspace
+exec jupyter-lab \
+    --allow-root \
+    --ip=0.0.0.0 \
+    --port=8888 \
+    --no-browser \
+    --notebook-dir="$WORK_DIR" \
+    --NotebookApp.token='' \
+    --NotebookApp.password='' \
+    --NotebookApp.base_url=${JUPYTERHUB_SERVICE_PREFIX} \
+    --NotebookApp.default_url=/lab
