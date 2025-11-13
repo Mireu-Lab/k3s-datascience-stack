d31b09d595871f8e40c68c8d22d7e42681c222e5
mireu-lab <limmireu@cu.ac.kr>
2025-11-13 10:30:32 +0000

    🗑️ chore: remove all deployment files
    - remove all deployment scripts and related files
    - cleanup project directory for a fresh start

--- deployments/storage/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Applying PersistentVolume and StorageClass configurations..."
-
-kubectl apply -f deployments/storage/storage-pv.yaml
-kubectl apply -f deployments/storage/storage-class.yaml
-
-echo "Storage setup complete."
